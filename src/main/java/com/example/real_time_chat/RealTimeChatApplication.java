21a197e2266a292075abd8804eee5f2b4d4e3c43
sunsik <sunsig21c@gmail.com>
2023-03-07 16:57:26 +0900

    프로젝트 빌드

---
+++ src/main/java/com/example/real_time_chat/RealTimeChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_time_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/real_time_chat/RealTimeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real_time_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
