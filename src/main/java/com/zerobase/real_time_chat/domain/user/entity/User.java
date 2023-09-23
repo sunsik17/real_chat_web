aa78566ae1cff70d8bb2d111715fc9d4924927de
sunsik <sunsig21c@gmail.com>
2023-09-23 19:41:28 +0900

    Refactor : N+1 문제 해결과정
    entity클래스에 @ToString 삭제

--- src/main/java/com/zerobase/real_time_chat/domain/user/entity/User.java
+++ src/main/java/com/zerobase/real_time_chat/domain/user/entity/User.java
@@ -11,14 +11,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@ToString
 @Entity
 public class User extends BaseTimeEntity {
 
